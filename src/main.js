2371a4ea22f5c9648c7ed91f0f9de2836fceef79
Abdulla Askar <63617982+abdullaaskar@users.noreply.github.com>
2020-04-22 11:50:16 -0700

    Add point comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+// Rocket Patrol Mod---Archer Simulater  
+// Created by Abudula Aisikaer
+
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (25)
+// Create a new title screen (15)
+// Display the time remaining (in seconds) on the screen (15)
+// Allow the player to control the Rocket after it's fired (10)
+// Add your own (copyright-free) background music to the Play scene (10)
+// Create a new scrolling tile sprite for the background (10)
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
